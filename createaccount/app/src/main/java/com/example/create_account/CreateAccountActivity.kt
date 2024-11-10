1f0636342c68949cf39b1fa6b1a48eb9aee9a4d5
Sofiia <sofiiavoitkova@gmail.com>
2024-11-10 17:08:05 +0100

    Add navigation for Log in and Register actvities with backstack management

---
+++ createaccount/app/src/main/java/com/example/create_account/CreateAccountActivity.kt
@@ -0,0 +1,20 @@
+package com.example.create_account
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+        val registerNowText = findViewById<TextView>(R.id.logInText)
+        registerNowText.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+    }
+}
