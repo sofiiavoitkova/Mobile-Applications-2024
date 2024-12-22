e9efda02bf74e401ff6d4ab151f513a9e91b3b80
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-12-22 22:10:30 +0100

    feat: implement RecyclerView for recipes

--- app/src/main/java/com/example/create_account/FragmentA.kt
+++ app/src/main/java/com/example/create_account/FragmentA.kt
@@ -3,4 +3,11 @@ package com.example.create_account
 import androidx.fragment.app.Fragment
 import com.example.accountcreate.R
 
-class FragmentA : Fragment(R.layout.fragment_a)
+class FragmentA : Fragment(R.layout.fragment_a) {
+
+    private var eventListener: EventListener? = null
+
+    interface EventListener {
+        fun onGoToBPressed()
+    }
+}
